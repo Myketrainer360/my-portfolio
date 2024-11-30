a5d9b9fc3360fb3ad4646ee812298dd32dbdba5d
Myketrainer360 <133960488+Myketrainer360@users.noreply.github.com>
2024-11-30 09:45:14 +0100

    My first Deploy on Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
+});
